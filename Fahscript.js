771d88c7f778a42c9b801deb2d2624e94c6a0cfb
Jane Sferrazza <jane@Janes-MBP.localdomain>
2020-12-21 11:13:11 -0500

    commit

---
+++ Fahscript.js
@@ -0,0 +1,8 @@
+var farenheit = 
+Number(prompt("Please Enter a number to be converted from farenheit to celsuis:"));
+var celsuis = (farenheit - 32) / 1.8000;
+alert("Your farenheit number has been converted to celsuis which is : " + celsuis + " Celsuis");
+
+/* °C = °F - 32 
+              ______  
+              1.8000  */
---
+++ picascript.js
@@ -0,0 +1,4 @@
+var pica = 
+Number(prompt("Please Enter a number to be converted from Pica to pixels:"));
+var pixels = (pica)* 16 ;
+alert("Your pica to pixels converted : " + pixels + " pixels");
